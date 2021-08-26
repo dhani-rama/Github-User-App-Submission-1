36137ff34688caf841a1f190c168ea9d905b5cb4
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2021-08-27 06:24:39 +0700

    Add splash screen activity

---
+++ app/src/main/java/id/research/githubuserapp/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package id.research.githubuserapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
