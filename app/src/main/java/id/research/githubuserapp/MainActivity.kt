1f62bdcaea425629458bce43eea89d4a820f4386
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2021-09-01 18:54:28 +0700

    fix logic and layout activity main and splash screen activity

--- app/src/main/java/id/research/githubuserapp/MainActivity.kt
+++ app/src/main/java/id/research/githubuserapp/MainActivity.kt
@@ -1,11 +1,36 @@
 package id.research.githubuserapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import id.research.githubuserapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mMainBinding: ActivityMainBinding
+    private lateinit var mUsers: ArrayList<UsersEntity>
+    private lateinit var mUsersGithubAdapter: UsersGithubAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mMainBinding.root)
+
+        mUsersGithubAdapter = UsersGithubAdapter()
+        mUsers = arrayListOf<UsersEntity>()
+
+        showList()
+    }
+
+    private fun showList() {
+
+        mUsers.addAll(UsersData.listData)
+        mUsersGithubAdapter.setListUsers(mUsers)
+
+        with(mMainBinding.rvUser) {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            setHasFixedSize(true)
+            adapter = mUsersGithubAdapter
+        }
     }
 }
--- app/src/main/java/id/research/githubuserapp/SplashScreenActivity.kt
+++ app/src/main/java/id/research/githubuserapp/SplashScreenActivity.kt
@@ -1,11 +1,24 @@
 package id.research.githubuserapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import id.research.githubuserapp.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var mSplashBinding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        mSplashBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(mSplashBinding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
